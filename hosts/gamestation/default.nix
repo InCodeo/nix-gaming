01654abfbfd12bb6058be98b689797b5864f655a
InCodeo <githublife@proton.me>
2024-12-09 10:53:30 +1100

    Update default.nix

--- hosts/gamestation/default.nix
+++ hosts/gamestation/default.nix
@@ -1,44 +1,53 @@
-{ config, pkgs, lib, ... }: {
-  # Disable all default NVIDIA configuration
-  disabledModules = [ "hardware/video/nvidia.nix" ];
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Then configure exactly what we want
+  # Add NVIDIA configuration with broken packages allowed
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowBroken = true;
+    nvidia.acceptLicense = true;
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  boot = {
+    kernelPackages = lib.mkForce pkgs.linuxPackages_6_1;
+    kernelModules = [ "nvidia" ];
+    extraModulePackages = [ config.boot.kernelPackages.nvidia_x11_legacy470 ];
+  };
+
+  # NVIDIA configuration for GTX 970
   hardware.nvidia = {
-    open = lib.mkForce false;
+    open = false;
     nvidiaSettings = true;
-    # Only allow legacy driver
-    package = lib.mkForce (lib.hiPrio pkgs.linuxPackages_6_1.nvidia_x11_legacy470);
+    package = lib.mkForce config.boot.kernelPackages.nvidia_x11_legacy470;
     modesetting.enable = true;
     prime.sync.enable = false;
     powerManagement.enable = false;
     forceFullCompositionPipeline = true;
   };
 
-  boot = {
-    kernelPackages = lib.mkForce pkgs.linuxPackages_6_1;
-    blacklistedKernelModules = [ "nouveau" "nvidia_drm" "nvidia_modeset" "nvidia" ];
-    kernelModules = [ ];
-    extraModulePackages = [ ];
-    # Add this after initial module load
-    initrd.kernelModules = [ "nvidia" ];
-    extraModprobeConfig = ''
-      options nvidia NVreg_RegisterForACPIEvents=1
-    '';
-  };
+  networking.hostName = "gamestation";
 
-  # Only allow specific version in nixpkgs
-  nixpkgs.config = {
-    allowUnfree = true;
-    allowBroken = true;
-    nvidia.acceptLicense = true;
-    # Prevent other NVIDIA packages from being built
-    packageOverrides = pkgs: {
-      linuxPackages_6_1 = pkgs.linuxPackages_6_1.override {
-        nvidia_x11 = lib.mkForce null;
-      };
+  services = {
+    fstrim.enable = true;
+    xserver = {
+      enable = true;
+      videoDrivers = ["nvidia"];
+      displayManager.gdm.wayland = true;
     };
   };
 
-  # Make sure X.org knows to use our specific driver
-  services.xserver.videoDrivers = lib.mkForce [ "nvidia" ];
+  environment.systemPackages = with pkgs; [
+    nvtopPackages.full
+    glxinfo
+    vulkan-tools
+  ];
 }
