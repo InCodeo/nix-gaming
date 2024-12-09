71083b2045509856e4732697a5c602427f044486
InCodeo <githublife@proton.me>
2024-12-09 21:37:59 +1100

    Update default.nix

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,9 +1,8 @@
 # networking configuration
-{
+{ pkgs, ... }: {
   networking = {
     # use quad9 with DNS over TLS
     nameservers = ["9.9.9.9#dns.quad9.net"];
-
     networkmanager = {
       enable = true;
       dns = "systemd-resolved";
